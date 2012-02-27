e2fbd68fa1634e5b86baa641729c0bf684235bf3
Christopher John <cojohn@gmail.com>
2012-02-26 22:54:32 -0500

    [feature] rough batch scheduling for issue #3

--- test/client.js
+++ test/client.js
@@ -1,15 +1,11 @@
 var leeloo = require("../lib/leeloo");
 
 leeloo.connect(1997, function(error) {
-	leeloo.dispatch("list", function(error, response) {
-		leeloo.dispatch("schedule", function(error, response) {
 
-			leeloo.disconnect(function(error) {
+	leeloo.dispatch("scheduleJobs", [{"w": Date.now() + 10000},{"w": Date.now() + 20000},{"w": Date.now() + 30000}]);
 
-			});
-
-		});		
-
+	leeloo.on("scheduledJobs", function(jobs) {
+		console.log(jobs);
 	});
-	
+
 });
