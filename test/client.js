d8ceeaa5640a13362f130a1bcf44ee86eb11a5dc
Christopher John <cojohn@gmail.com>
2012-02-29 12:08:51 -0500

    [feature] jobs reschedule when finished

--- test/client.js
+++ test/client.js
@@ -2,10 +2,11 @@ var leeloo = require("../lib/leeloo");
 
 leeloo.connect(1997, function(error) {
 
-	leeloo.dispatch("scheduleJobs", [{"w": Date.now() + 10000},{"w": Date.now() + 20000, "xid": 2},{"w": Date.now() + 30000}]);
+	//leeloo.dispatch("scheduleJobs", [{"w": Date.now() + 10000},{"w": Date.now() + 20000, "xid": 2},{"w": Date.now() + 30000}]);
+	leeloo.dispatch("schedule", {"when": Date.now() + 1000, "reschedule": 1000, "xid": 1});
 
-	leeloo.on("scheduledJobs", function(jobs) {
-		console.log(jobs);
+	leeloo.on("processJob", function(job) {
+		console.log("Received a finish job for " + job._id);
+		leeloo.dispatch("finishJob", {"id": job._id});					
 	});
-
 });
