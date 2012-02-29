5a2d465505f25511c4df1b71f0aeab274dbcb19b
Christopher John <cojohn@gmail.com>
2012-02-29 11:23:13 -0500

    [feature] push back same xid/type for #5
    [feature] better error handling for #3
    [docs] version bump for `scheduleJobs` and modified `schedule`

--- test/client.js
+++ test/client.js
@@ -2,7 +2,7 @@ var leeloo = require("../lib/leeloo");
 
 leeloo.connect(1997, function(error) {
 
-	leeloo.dispatch("scheduleJobs", [{"w": Date.now() + 10000},{"w": Date.now() + 20000},{"w": Date.now() + 30000}]);
+	leeloo.dispatch("scheduleJobs", [{"w": Date.now() + 10000},{"w": Date.now() + 20000, "xid": 2},{"w": Date.now() + 30000}]);
 
 	leeloo.on("scheduledJobs", function(jobs) {
 		console.log(jobs);
