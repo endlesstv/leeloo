a3997f19c6a6dfef9d72000684c810da48af7d37
Christopher John <cojohn@gmail.com>
2012-05-01 16:21:53 -0400

    [feature] breaking client into its own file

---
+++ index.js
@@ -0,0 +1,2 @@
+exports.Server = require("./lib/leeloo");
+exports.Client = require("./lib/leeloo-client");
--- test/client.js
+++ test/client.js
@@ -1,12 +1,21 @@
-var leeloo = require("../lib/leeloo");
+var Client = require("../index").Client,
+	Server = require("../index").Server;
 
-leeloo.connect(1997, function(error) {
-
-	//leeloo.dispatch("scheduleJobs", [{"w": Date.now() + 10000},{"w": Date.now() + 20000, "xid": 2},{"w": Date.now() + 30000}]);
-	leeloo.dispatch("schedule", {"when": Date.now() + 1000, "reschedule": 1000, "xid": 1});
-
-	leeloo.on("processJob", function(job) {
-		console.log("Received a finish job for " + job._id);
-		leeloo.dispatch("finishJob", {"id": job._id});					
+Server.startServer(function(error) {
+	var client = new Client();
+	
+	client.on("connected", function() {
+		console.log("Connected.");
+		client.disconnect();
 	});
-});
+	
+	client.on("disconnected", function() {
+		// You could put code here to poll and reconnect.
+		console.log("Disconnected.");
+		Server.stopServer(function(error) {
+			process.exit();
+		});
+	});
+	
+	client.connect(1997);
+});	
